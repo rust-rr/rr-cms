930e6c7a619153ac6e765f2ac1056137e2f28c63
RenXusheng233 <782449909@qq.com>
2024-05-06 20:47:25 +0800

    chore(server): update

--- server/examples/quick_dev.rs
+++ server/examples/quick_dev.rs
@@ -3,7 +3,11 @@ use anyhow::Result;
 #[tokio::main]
 async fn main() -> Result<()> {
     let hc = httpc_test::new_client("http://127.0.0.1:8080")?;
-    hc.do_get("/hello").await?.print().await?;
+    hc.do_get("/hello?name=Richard").await?.print().await?;
+
+    hc.do_get("/hello2/Richard").await?.print().await?;
+    
+    // hc.do_get("/src/main.rs").await?.print().await?;
 
     Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,18 +1,52 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    extract::{Path, Query},
+    response::{Html, IntoResponse},
+    routing::{get, get_service},
+    Router,
+};
+use serde::Deserialize;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
+
+pub use self::error::{Error, Result};
+
+mod error;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hello <strong>World</strong>") }),
-    );
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_static());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     let listener = TcpListener::bind(addr).await.unwrap();
     println!("Listening on http://{addr}\n");
-    axum::serve(listener, routes_hello.into_make_service())
-        .await
-        .unwrap();
+    axum::serve(listener, routes_all).await.unwrap();
+}
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+// e.g., /hello?name=Richard
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {:<?}", "HANDLER", params);
+    let name = params.name.as_deref().unwrap_or("World!");
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+// e.g., /hello/Richard
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 - {}", "HANDLER", name);
+    Html(format!("Hello <strong>{name}</strong>"))
 }
